ecbfbc6e386fc8a4d37ea1e0706e19a65327a4dc
acelaena <gildedserpentdesigns@gmail.com>
2020-03-24 02:36:49 -0700

    updates

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
 To do list
 
-Overlays on all the images in illustration
 tagging system on illustration
 
 zoom feature on illustration
 
-Sketch, Render, Painting, Lined, Traditional Art, Chibi, Character Design, Fanart
 
-MDZS, Fire Emblem, Pokemon, Kingdom Hearts, Fate Grand Order, Dragalia Lost 
 
 project page for shrouded kingdom
 prject page for pokemon eden
--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 /*Open-close popup script*/
-"use strict";
-
 function popup(id) {
+    "use strict";
     var element = document.getElementById(id);
     document.getElementById("background").style.display = "block";
     element.style.display = "block";
@@ -9,6 +8,7 @@ function popup(id) {
 }
 
 function closePopup() {
+    "use strict";
     document.getElementById("background").style.display = "none";
     document.getElementById("sketch").style.display = "none";
     document.getElementById("chibi").style.display = "none";
@@ -17,3 +17,29 @@ function closePopup() {
     document.getElementById("painted").style.display = "none";
 }
 
+function closeArtbox() {
+    "use strict";
+    document.getElementById("background").style.display = "none";
+}
+
+function showArtbox(art, title, description, tags) {
+
+    "use strict";
+    //Set background display
+    document.getElementById("background").style.display = "block";
+    
+    //Set artbox art
+    var url = "url('../portfolio/gallery/" + art + "')";
+    document.getElementById("box").style.backgroundImage = url;
+    
+    
+    //Set title 
+    document.getElementById("title").innerHTML = title;
+    
+    //set description
+    document.getElementById("desc").innerHTML = description;
+    
+    //set tags
+    document.getElementById("tags").innerHTML = tags;
+    
+}
